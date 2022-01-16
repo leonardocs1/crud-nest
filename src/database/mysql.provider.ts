7bf41e501728cd190084ea4d20931029ab5a263d
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2022-01-16 12:44:31 -0300

    Global module

--- src/database/mysql.provider.ts
+++ src/database/mysql.provider.ts
@@ -1,13 +1,25 @@
-import { Injectable, Logger } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common"
+import {createPool, Pool} from 'mysql2/promise'
 
 @Injectable()
 export class MySQLProvider {
   private readonly logger : Logger
+  private readonly pool: Pool
   constructor() {
     this.logger = new Logger('MySQLProvider')
+    this.pool = createPool({
+      host: 'localhost',
+      user: 'root',
+      database: 'cat-products',
+      waitForConnections: true,
+      connectionLimit: 20,
+      queueLimit: 0
+  })
     this.logger.log('Initialized!')
   }
-  getValue(): string {
-    return 'Value from MySQL Provider!'
+  async getValue(): Promise<string> {
+    const conn = await this.pool.getConnection()
+    const [results] = await conn.query('select * from products')
+    return JSON.stringify(results)
   }
 }
