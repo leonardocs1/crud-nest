de818de041dda8d21086e948be76f2b1c3845fd4
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2022-01-15 22:50:32 -0300

    Providers

--- src/module/product/product.module.ts
+++ src/module/product/product.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
+import { MySQLProvider } from 'src/database/mysql.provider';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 
 @Module({
   controllers: [ProductController],
-  providers: [ProductService]
+  providers: [ProductService, MySQLProvider]
 })
 export class ProductModule {}
