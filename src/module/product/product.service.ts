36d4235f8a7cacdcfdf73df6e48848dfb947f273
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2022-01-15 22:27:58 -0300

    Controllers e Services

---
+++ src/module/product/product.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class ProductService {
+  async findAll(): Promise<any> {
+    return [
+      {id: 1},
+      {id: 2},
+      {id: 3},
+      {id: 4},
+    ]
+  }
+}
